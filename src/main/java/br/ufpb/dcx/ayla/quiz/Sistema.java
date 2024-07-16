908ad702b4008fb78befebc5f3381a6e53037ee2
brunobrunnettufpb <bruno.mangueira@dcx.ufpb.br>
2024-07-16 09:57:00 -0300

    h

--- src/main/java/br/ufpb/dcx/ayla/quiz/Sistema.java
+++ src/main/java/br/ufpb/dcx/ayla/quiz/Sistema.java
@@ -1,10 +1,27 @@
 package br.ufpb.dcx.ayla.quiz;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
+
 public class Sistema {
     public static void main(String[] args) {
         System.out.println("Oi");
 
         Pergunta pergunta = new PerguntaDissertativa();
         System.out.println(pergunta.toString());
+
+        Pergunta pergunta2 = new PerguntaDissertativa("Quanto é 2+2?", "4");
+        Pergunta pergunta3 = new PerguntaDissertativa("Quanto é 1+1?", "2");
+
+        List<Pergunta> perguntasList = new Vector();
+        perguntasList.add(pergunta);
+        perguntasList.add(pergunta2);
+        perguntasList.add(pergunta3);
+        Collections.sort(perguntasList);
+
+        for (Pergunta p: perguntasList) {
+
+        }
     }
 }
