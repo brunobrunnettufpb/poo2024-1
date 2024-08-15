5e76cb07d8209b78e6aa328dc788c342f228754b
bruno brunnett <bruno.mangueira@dcx.ufpb.br>
2024-08-15 10:52:14 -0300

    exerc06

--- src/main/java/br/ufpb/dcx/poo2024_1/exerc06/GravadorDeDados.java
+++ src/main/java/br/ufpb/dcx/poo2024_1/exerc06/GravadorDeDados.java
@@ -1,16 +1,21 @@
 package br.ufpb.dcx.poo2024_1.exerc06;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class GravadorDeDados {
     public static final String ARQUIVO_CONTATOS = "contatos.dat";
 
-    public HashMap<String, Contato> recuperarContatos() {
+    public HashMap<String, Contato> recuperarContatos() throws IOException {
+        HashMap<String, Contato> contatos = new HashMap<>();
 
+
+
+        return contatos;
     }
 
-    public void salvarContatos(Map<String, Contato> contatos) {
+    public void salvarContatos(HashMap<String, Contato> contatos) {
 
     }
 }
