2e8aec6a99831d1a1b199cc68b5693c14429f8cf
bruno brunnett <bruno.mangueira@dcx.ufpb.br>
2024-08-22 10:29:56 -0300

    JFrame Swing

---
+++ src/main/java/br/ufpb/dcx/poo2024_1/exerc06/AgendaGUI.java
@@ -0,0 +1,15 @@
+package br.ufpb.dcx.poo2024_1.exerc06;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AgendaGUI extends JFrame {
+    public AgendaGUI() {
+        setTitle("Agenda de Aniversários");
+        setSize(400,200);
+        setLocationRelativeTo(null);
+        //setLocation(150,150);
+        setResizable(false);
+        getContentPane().setBackground(Color.lightGray);
+    }
+}
